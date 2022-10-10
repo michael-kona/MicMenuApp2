af1d32e61f425df7d4d782c8f48e390d352b6e5b
michael-kona <michael.kona@gmail.com>
2022-10-10 16:27:04 +0530

    Initial commit

---
+++ app/src/main/java/com/example/micmenuapp2/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.micmenuapp2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+ImageView imgv;
+TextView txtv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imgv=findViewById(R.id.imageView);
+        registerForContextMenu(imgv);
+    }
+    public void txtv_click(View v)
+    {
+        Toast.makeText(this, "popup", Toast.LENGTH_SHORT).show();
+        txtv=findViewById(R.id.textView);
+        PopupMenu ppm=new PopupMenu(this,txtv);
+        MenuInflater mi=getMenuInflater();
+        mi.inflate(R.menu.micpopup,ppm.getMenu());
+        ppm.show();
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo)
+    {
+        MenuInflater mi=getMenuInflater();
+        mi.inflate(R.menu.miccontext,menu);
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item)
+    {
+        Toast.makeText(this, "context", Toast.LENGTH_SHORT).show();
+        switch (item.getItemId())
+        {
+            case R.id.ai1:imgv.setImageResource(R.drawable.ai_1);
+                            break;
+            case R.id.ai2:imgv.setImageResource(R.drawable.ai_2);
+                break;
+            case R.id.ai3:imgv.setImageResource(R.drawable.ai_3);
+                break;
+            default:imgv.setImageResource(R.drawable.ai_3);
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater mi=getMenuInflater();
+        mi.inflate(R.menu.micoptions,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        Toast.makeText(this,item.getTitle() , Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
+    }
+}
